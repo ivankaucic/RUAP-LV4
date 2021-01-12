14d777f8e23a01e9f271e3b31796d38ac7c682d3
Nikola <nstipic@etfos.hr>
2021-01-12 19:27:55 +0100

    gotov zadatak

---
+++ RUAP_LV4/RUAP_LV4/UnitTest1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class Prijava
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ThePrijavaTest()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/");
+            driver.FindElement(By.XPath("//div[@id='top-links']/ul/li[2]/a/span[2]")).Click();
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("input-email")).Click();
+            driver.FindElement(By.Id("input-email")).Clear();
+            driver.FindElement(By.Id("input-email")).SendKeys("ivan.kaucic01@gmail.com");
+            driver.FindElement(By.Id("input-password")).Click();
+            driver.FindElement(By.Id("input-password")).Clear();
+            driver.FindElement(By.Id("input-password")).SendKeys("Dz9feen9");
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+        [Test]
+        public void ThePromjenaValuteTest()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/index.php?route=common/home");
+            driver.FindElement(By.LinkText("Show All Desktops")).Click();
+            driver.FindElement(By.XPath("//form[@id='form-currency']/div/button")).Click();
+            driver.FindElement(By.Name("EUR")).Click();
+            driver.FindElement(By.LinkText("Show All Desktops")).Click();
+            driver.FindElement(By.XPath("//form[@id='form-currency']/div/button")).Click();
+            driver.FindElement(By.Name("GBP")).Click();
+        }
+        [Test]
+        public void TheShoppingCartTest()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/index.php?route=common/home");
+            driver.FindElement(By.LinkText("My Account")).Click();
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("input-email")).Click();
+            driver.FindElement(By.Id("input-email")).Clear();
+            driver.FindElement(By.Id("input-email")).SendKeys("ivan.kaucic01@gmail.com");
+            driver.FindElement(By.Id("input-password")).Click();
+            driver.FindElement(By.Id("input-password")).Clear();
+            driver.FindElement(By.Id("input-password")).SendKeys("Dz9feen9");
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            driver.FindElement(By.LinkText("Your Store")).Click();
+            driver.FindElement(By.LinkText("Desktops")).Click();
+            driver.FindElement(By.LinkText("Show All Desktops")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']/div[4]/div[5]/div/div[2]/div[2]/button/span")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']/div[4]/div[6]/div/div[2]/div[2]/button/span")).Click();
+            driver.FindElement(By.XPath("//div[@id='product-category']/ul/li/a/i")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Desktops')])[3]")).Click();
+        }
+
+        [Test]
+        public void TheWishListTest()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/index.php?route=common/home");
+            driver.FindElement(By.XPath("//div[@id='top-links']/ul/li[2]/a/span")).Click();
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("input-email")).Click();
+            driver.FindElement(By.Id("input-email")).Clear();
+            driver.FindElement(By.Id("input-email")).SendKeys("ivan.kaucic01@gmail.com");
+            driver.FindElement(By.XPath("//div[@id='content']/div/div[2]/div/form/div[2]")).Click();
+            driver.FindElement(By.Id("input-password")).Click();
+            driver.FindElement(By.Id("input-password")).Clear();
+            driver.FindElement(By.Id("input-password")).SendKeys("Dz9feen9");
+            driver.FindElement(By.Id("input-password")).SendKeys(Keys.Enter);
+            driver.FindElement(By.LinkText("Your Store")).Click();
+            driver.FindElement(By.LinkText("Show All Desktops")).Click();
+            driver.FindElement(By.XPath("(//button[@type='button'])[10]")).Click();
+            driver.FindElement(By.XPath("//a[@id='wishlist-total']/span")).Click();
+        }
+
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
